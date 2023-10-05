35f29bfaf9638e78bbd87de4905bb9fef2833e36
Gowthamkumar-R <gowtham248999@gmail.com>
2023-10-05 10:20:31 +0530

    First Commit

---
+++ src/test/java/Jenkins/Jenkins/AppTest.java
@@ -0,0 +1,22 @@
+package Jenkins.Jenkins;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	
+	public void Sample() {
+		
+		System.out.println("Jenkin starts running");
+		int a=10,b=90,c;
+		c=a+b;
+		System.out.println("Total output is " + c);
+		
+	}
+	public static void main(String[] args) {
+		
+		AppTest obj = new AppTest();
+		obj.Sample();
+		
+	}
+   
+}
